000148b38004537d2da149014159e5cd515f6a94
HummamShah <humamshah11@gmail.com>
2020-12-14 13:22:56 +0500

    Roles added and notification table added in db

---
+++ LMS/Models/Enums/Roles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS.Models.Enums
+{
+    public static class Roles
+    {
+        public static string Lead = "Lead";
+        public static string Lead_Sales = "Lead_Sales";
+        public static string Pmd = "Pmd";
+        public static string PreSale = "PreSale";
+        public static string Closer = "Closer";
+        public static string Admin = "Admin";
+        public static string SuperAdmin = "SuperAdmin";
+    }
+}
---
+++ LMSDatabase/dbo/Tables/Notification.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Notification]
+(
+	[Id] INT NOT NULL PRIMARY KEY Identity(1,1),
+	[AgentId] int,
+	[Content] nvarchar(max),
+	[IsActive] bit,
+	[CreatedBy] nvarchar(max),
+	[CreatedAt] DateTime,
+	[UpdatedBy] nvarchar(max),
+	[UpdatedAt] DateTime,
+	Constraint [FK_Notification_Agent] foreign key ([AgentId]) References [dbo].[Agent] ([Id])
+)
